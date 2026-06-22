623457de77b7e3a6ff9255eb062ee99429525a9c
pshs-demonlist <b2031sltsantillan@pshs.edu.ph>
2026-06-22 16:03:57 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// js/config.js
+export const CONFIG = {
+  API: {
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyq373Puku6D14WhdN0lL7Z7_CdBJ8pycTgIGEUEJFBAzsnpNbQ17Nx6hOtfPHJRd5ASA/exec",
+    GITHUB_PRIMARY_URL: "https://raw.githubusercontent.com/pshs-demonlist/pshsmc-demonlist/main/levels.json",
+    GITHUB_CDN_URL: "https://cdn.jsdelivr.net/gh/pshs-demonlist/pshsmc-demonlist@main/levels.json",
+    FETCH_TIMEOUT_MS: 8000
+  },
+  IMAGES: {
+    FALLBACK_THUMBNAIL: 'https://img.youtube.com/vi/a83hV1_3B88/maxdefault.jpg'
+  }
+};
